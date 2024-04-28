8ef9a3f7b69c2a4127400750b8399acfb81d9d60
joshkeselman <jhkeselman@wpi.edu>
2024-04-28 11:01:58 -0400

    Refactored stuff

--- main/move_robot.m
+++ main/move_robot.m
@@ -1,19 +1,5 @@
-function tau = move_robot(robot, params)
-% This will take in a set of parameters from the GUI including the robot,
-% the desired pose and wrench, and returns all the relevant info for
-% plotting the robot and the graphs. we can probably move this to live in
-% the GUI if we want (or just keep it here for visual ease)
-    x = params.x;
-    y = params.y;
-    z = params.z;
-    roll = params.roll;
-    pitch = params.pitch;
-    yaw = params.yaw;
-    wrench = params.wrench;
-    lx = params.lx;
-    ly = params.ly;
-    lz = params.lz;
-    lroll = params.lroll;
-    lpitch = params.lpitch;
+function tau = move_robot(robot, currentQ, targetQ, force)
+% Takes in the robot, the current joints nad hte target points. charts the
+% path between the two
     tau = [];
 end
